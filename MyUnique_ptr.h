9df10d7541a0d658bef24779369b49a75a0d8621
Aleksandr Dziuba <dziubaaleksandr22@gmail.com>
2021-10-12 15:23:14 +0000

    Add move assignment

--- MyUnique_ptr.h
+++ MyUnique_ptr.h
@@ -6,11 +6,12 @@ class MyUnique_ptr
     private:
     T* m_ptr;
     public:
-    explicit MyUnique_ptr(T* ptr = nullptr): m_ptr(ptr){}
+    explicit MyUnique_ptr(T* ptr = nullptr): m_ptr(ptr){};
     MyUnique_ptr(MyUnique_ptr& uptr) = delete;
     MyUnique_ptr(MyUnique_ptr&& uptr);
     T*operator ->(){ return this->m_ptr;}
     T& operator*() { return *(this->m_ptr);}
+    void operator=(MyUnique_ptr&& uptr);
     ~MyUnique_ptr()
     {
         if ( m_ptr != nullptr)
@@ -20,3 +21,18 @@ class MyUnique_ptr
     }
 };
 
+template<class T>
+MyUnique_ptr<T>::MyUnique_ptr (MyUnique_ptr&& uptr)
+{
+    m_ptr = uptr.m_ptr;
+    uptr.m_ptr = nullptr;
+}
+
+template<class T>
+void MyUnique_ptr<T>::operator=(MyUnique_ptr&& uptr) // move assignment
+{
+	if (m_ptr != nullptr) delete m_ptr;
+
+	m_ptr = uptr.m_ptr;
+	uptr.m_ptr = nullptr;
+}
