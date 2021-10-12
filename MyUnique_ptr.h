5aa54448a944caa0fbe8d34cff60710b355e528d
Aleksandr Dziuba <dziubaaleksandr22@gmail.com>
2021-10-12 15:33:08 +0000

    Fix * operator

--- MyUnique_ptr.h
+++ MyUnique_ptr.h
@@ -10,15 +10,9 @@ class MyUnique_ptr
     MyUnique_ptr(MyUnique_ptr& uptr) = delete;
     MyUnique_ptr(MyUnique_ptr&& uptr);
     T*operator ->(){ return this->m_ptr;}
-    T& operator*() { return *(this->m_ptr);}
+    T& operator*();
     void operator=(MyUnique_ptr&& uptr);
-    ~MyUnique_ptr()
-    {
-        if ( m_ptr != nullptr)
-        {
-            delete m_ptr;
-        }
-    }
+    ~MyUnique_ptr();
 };
 
 template<class T>
@@ -35,4 +29,20 @@ void MyUnique_ptr<T>::operator=(MyUnique_ptr&& uptr) // move assignment
 
 	m_ptr = uptr.m_ptr;
 	uptr.m_ptr = nullptr;
+}
+
+template<class T>
+MyUnique_ptr<T>::~MyUnique_ptr()
+{
+    if ( m_ptr != nullptr)
+    {
+        delete m_ptr;
+    }
+}
+
+template<class T>
+T& MyUnique_ptr<T>::operator*()
+{
+    if(m_ptr == nullptr) throw std::string("Nullptr");
+    return *(this->m_ptr);
 }
